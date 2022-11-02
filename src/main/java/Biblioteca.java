98259d78c24b44aaff52f772b89b2906eef7b97f
Bevilhonda <marcelinyuri@gmail.com.br>
2022-11-02 19:39:45 -0300

    Adiciona Classe Biblioteca e seus testes

---
+++ src/main/java/Biblioteca.java
@@ -0,0 +1,32 @@
+import java.util.List;
+
+public class Biblioteca {
+
+    private final String nome_da_biblioteca;
+    private final List<Livro> lista_de_livro;
+    private final Endereco endereco;
+    private final int id_biblioteca;
+
+    public Biblioteca(String nome, List<Livro> lista_de_livro, Endereco endereco, int id_Biblioteca) {
+        this.nome_da_biblioteca = nome;
+        this.lista_de_livro = lista_de_livro;
+        this.endereco = endereco;
+        this.id_biblioteca = id_Biblioteca;
+    }
+
+    public String getNome_da_biblioteca() {
+        return nome_da_biblioteca;
+    }
+
+    public List<Livro> getLista_de_livro() {
+        return lista_de_livro;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public int getId_biblioteca() {
+        return id_biblioteca;
+    }
+}
---
+++ src/test/java/BibliotecaTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BibliotecaTest {
+
+    @Test
+    public void Test_Construtor() {
+
+        List<Livro> livro_atual = new ArrayList<>();
+
+        Endereco novo_endereco = new Endereco
+                ("Afonso", 25, "Centro", "Maringá", "Paraná", 1);
+
+        Biblioteca nova_biblioteca = new Biblioteca("Sarandi", livro_atual, novo_endereco, 1);
+        assertEquals("Sarandi",nova_biblioteca.getNome_da_biblioteca());
+        assertEquals(livro_atual,nova_biblioteca.getLista_de_livro());
+        assertEquals(novo_endereco,nova_biblioteca.getEndereco());
+        assertEquals(1,nova_biblioteca.getId_biblioteca());
+
+    }
+
+}
