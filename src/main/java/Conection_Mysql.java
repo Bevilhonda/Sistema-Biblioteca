3fc9314638c2eedeb9a1e973b8857a09ca7885a7
Bevilhonda <marcelinyuri@gmail.com.br>
2022-11-02 19:44:38 -0300

    Adiciona classe de conexão com banco de dados

---
+++ src/main/java/Conection_Mysql.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conection_Mysql {
+
+    public Connection getconection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection
+                    ("jdbc:mysql://localhost:3306/BibliotecaMaringa", "root", "@Bancodados2022");
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+}
