17c8befed4bf942da2501039ad87f5bd5a758431
Bevilhonda <marcelinyuri@gmail.com.br>
2022-11-08 22:02:55 -0300

    comando para um usuário selecionar um livro no banco através do id do livro

---
+++ src/main/java/Pesquisa_LivroBanco.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Pesquisa_LivroBanco {
+    int id_livro;
+
+    public void Seleciona_LivroBanco() {
+        String busca_livro = "select * from Livro where id_Livro = ? ";
+
+        try {
+            Scanner input_id_livro = new Scanner(System.in);
+            System.out.println("Digite um numero de id do banco de Livros :");
+            id_livro = input_id_livro.nextInt();
+
+            Conection_Mysql conecta = new Conection_Mysql();
+
+            Connection conecta_banco = conecta.getconection();
+
+            PreparedStatement seleciona_livro = conecta_banco.prepareStatement(busca_livro);
+            seleciona_livro.setInt(1,getId_livro());
+
+            ResultSet mostra_livro = seleciona_livro.executeQuery();
+            while (mostra_livro.next()){
+                System.out.println(mostra_livro.getString("id_Livro"));
+                System.out.println(mostra_livro.getString("titulo"));
+                System.out.println(mostra_livro.getInt(3));
+                System.out.println(mostra_livro.getDate( 6));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public int getId_livro() {
+        return id_livro;
+    }
+}
